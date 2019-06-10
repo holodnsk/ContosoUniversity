78fcb2880cf93b4937574a03b1f1e2f894159b68
kholodkov <kholodkov@deskalerts.com>
2019-06-10 12:27:17 +0700

    add course assigment to pages edit instructor and create instructor
    https://docs.microsoft.com/ru-ru/aspnet/core/data/ef-rp/update-related-data?view=aspnetcore-2.2#add-course-assignments-to-the-instructor-edit-page

--- ContosoUniversity/Pages/Instructors/Edit.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Edit.cshtml.cs
@@ -10,7 +10,7 @@ using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class EditModel : PageModel
+    public class EditModel : InstructorCoursesPageModel
     {
         private readonly ContosoUniversity.Models.SchoolContext _context;
 
@@ -31,6 +31,7 @@ namespace ContosoUniversity.Pages.Instructors
 
             Instructor = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments).ThenInclude(i => i.Course)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
 
@@ -38,10 +39,11 @@ namespace ContosoUniversity.Pages.Instructors
             {
                 return NotFound();
             }
+            PopulateAssignedCourseData(_context, Instructor);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(int? id, string[] selectedCourses)
         {
             if (!ModelState.IsValid)
             {
@@ -50,6 +52,8 @@ namespace ContosoUniversity.Pages.Instructors
 
             var instructorToUpdate = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ThenInclude(i => i.Course)
                 .FirstOrDefaultAsync(s => s.ID == id);
 
             if (await TryUpdateModelAsync<Instructor>(
@@ -63,9 +67,13 @@ namespace ContosoUniversity.Pages.Instructors
                 {
                     instructorToUpdate.OfficeAssignment = null;
                 }
+                UpdateInstructorCourses(_context, selectedCourses, instructorToUpdate);
                 await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
-            return RedirectToPage("./Index");
+            UpdateInstructorCourses(_context, selectedCourses, instructorToUpdate);
+            PopulateAssignedCourseData(_context, instructorToUpdate);
+            return Page();
         }
 
         private bool InstructorExists(int id)
